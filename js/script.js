d70ef7bac4ecaadc0ca697381b925a6d6f5655a7
SergeyPunko <punkosergey1997@gmail.com>
2018-10-07 17:55:00 +0300

    add: avatar+skills

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let btn = document.getElementById("menu-btn");
+let ava = document.getElementById("avatar");
+btn.addEventListener("click", function(){
+	if(btn.classList.contains('btn-active')){
+		btn.classList.remove('btn-active');
+	} else btn.classList.add("btn-active")
+})
+window.onscroll=function(){
+	if(window.pageYOffset>100){
+		ava.style.opacity=(0.4);
+		ava.style.filter  = 'blur(0)';
+	}
+	if(window.pageYOffset>300){
+		ava.style.opacity=(0.35);
+	}
+	if(window.pageYOffset>500){
+		ava.style.opacity=(0.3);
+		ava.style.filter  = 'blur(10px)';
+	}
+	if(window.pageYOffset>700){
+		ava.style.opacity=(0.25);
+		ava.style.filter  = 'blur(15px)';
+	}
+	if(window.pageYOffset>1000){
+		ava.style.filter  = 'blur(20px)';
+	}
+}
