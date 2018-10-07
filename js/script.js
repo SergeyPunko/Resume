e73bfd19d5dbc7613a1230d27a87d78c6b57a0e2
SergeyPunko <punkosergey1997@gmail.com>
2018-10-07 20:47:19 +0300

    fix: skill-level and text

--- js/script.js
+++ js/script.js
@@ -13,6 +13,12 @@ btn.addEventListener("click", function(){
 })
 window.onscroll=function(){
 	if(window.pageYOffset>100){
+		document.getElementsByClassName("java")[0].style.width="0%";
+		document.getElementsByClassName("html-css")[0].style.width="0%";
+		document.getElementsByClassName("js")[0].style.width="0%";
+		document.getElementsByClassName("eng")[0].style.width="0%";
+		document.getElementsByClassName("flexbox")[0].style.width="0%";
+		document.getElementsByClassName("cssgrid")[0].style.width="0%";
 		ava.style.opacity=(0.4);
 		ava.style.filter  = 'blur(0)';
 	}
@@ -22,12 +28,20 @@ window.onscroll=function(){
 	if(window.pageYOffset>500){
 		ava.style.opacity=(0.3);
 		ava.style.filter  = 'blur(10px)';
+
 	}
 	if(window.pageYOffset>700){
 		ava.style.opacity=(0.25);
 		ava.style.filter  = 'blur(15px)';
+		document.getElementsByClassName("java")[0].style.width="20%";
+		document.getElementsByClassName("html-css")[0].style.width="70%";
+		document.getElementsByClassName("js")[0].style.width="60%";
+		document.getElementsByClassName("eng")[0].style.width="30%";
+		document.getElementsByClassName("flexbox")[0].style.width="60%";
+		document.getElementsByClassName("cssgrid")[0].style.width="70%";
 	}
 	if(window.pageYOffset>1000){
 		ava.style.filter  = 'blur(20px)';
+		
 	}
 }
